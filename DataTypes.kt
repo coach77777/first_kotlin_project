68a3b473d59451443319094ebfa654526eb34670
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-06-25 06:33:44 -0500

    Kotlin Fundamentals

---
+++ DataTypes.kt
@@ -0,0 +1,24 @@
+package com.example.first_kotlin_project
+
+fun main(args: Array<String>) {
+
+    var a:Boolean = true
+    var b:Char = 'r'
+    var c:Byte = 12 //  -128 to 127
+    var d:Short = 354 // -32,768 to 32,767
+    var e:Int = 100000 // -2 to the 31st TO 2 to the 31st -1
+    var f:Long = 789789789789L // -2 to the 63rd TO 2 to the 63rd -1
+    var g:Float = 5.789345789F //
+    var h:Double = 7.789345789456 //
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+
+
+}

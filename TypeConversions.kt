6e5fde879b7694d392ff1a011fc64cde7544b68d
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-06-25 07:17:58 -0500

    Kotlin Fundamentals

---
+++ TypeConversions.kt
@@ -0,0 +1,24 @@
+package com.example.first_kotlin_project
+
+fun main(args: Array<String>) {
+
+    // Double> Float > Long > Integer > Short> Byte
+
+    var x:Byte = 127
+    var y:Int = x.toInt()
+    var z:Double = y.toDouble()
+
+    println(x)
+    println(y)
+    println(z)
+
+    var a:Double = 132.32
+    var b:Int = a.toInt()
+    var c:Byte = b.toByte()
+
+    println(a)
+    println(b)
+    println(c)//data loss
+
+
+}

762e1d2c424eff8a19cbe0d2d3399e1b599fc51f
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-07-02 04:15:17 -0500

    kl

--- HelloWorld.kt
+++ HelloWorld.kt
@@ -2,7 +2,7 @@ package com.example.first_kotlin_project
 
 fun main(args: Array<String>) {
 
-  print("Hello World!++")
-  print("Hello World!++!!!!!!!!!!!!!!!!")
+  print("Hello World!")
+
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # first_kotlin_project
 
-Fundamentals of Kotlin programming language.
+Operators In Kotlin.
